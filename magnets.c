ea354f851b348343fe377757cd1edabe5740659d
Jihad <bdjihad1862@gmail.com>
2023-06-20 21:21:46 +0600

    codeforces problem magnets and even and odds are solved

---
+++ magnets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=1;
+    char a[3],b[3];
+    scanf("%d",&n);
+    scanf("%s",&a);
+    for(i=1;i<n;i++)
+    {
+        scanf("%s",&b);
+        if(a[0]!=b[0])
+        {
+            s++;
+            strcpy(a,b);
+        }
+    }
+    printf("%d",s);
+    return 0;
+}
