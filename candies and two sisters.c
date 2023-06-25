916b0d8ec3a389a0960ee61ef3941e0f7d728979
Jihad <bdjihad1862@gmail.com>
2023-06-25 17:05:15 +0600

    codeforces problem candies and two sisters solved

---
+++ candies and two sisters.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t,n,m;
+    scanf("%d",&t);
+    for(int i=0;i<t;++i)
+    {
+        scanf("%d",&n);
+        if(n%2==0)
+        {
+            m=n/2-1;
+              printf("%d\n",m);
+        }
+        else
+        {
+            m=n/2;
+              printf("%d\n",m);
+        }
+    }
+    return 0;
+}
