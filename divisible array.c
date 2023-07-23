690cc4c849ce4c523a9715a22e3073bd4d3c5b7f
Jihad <bdjihad1862@gmail.com>
2023-07-23 18:22:10 +0600

    codeforces solution divisible array

---
+++ divisible array.c
@@ -0,0 +1,25 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int t,n;
+    	scanf("%d",&t);
+    	for(int i=0;i<t;i++)
+    	{
+    		scanf("%d",&n);
+    		if(n%2!=0)
+    		{
+    			for(int j=1;j<=n;j++)
+    			{
+    				printf("%d ",j);
+    			}
+    		}
+    		else
+    		{
+    			for(int j=1;j<=n;j++)
+    			{
+    				printf("%d ",2*j);
+    			}
+    		}
+    	}
+    	return 0;
+    }
